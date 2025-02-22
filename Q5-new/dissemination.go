0af9fcfa93505e4d86ec5c2883e24853c13bd46f
Pranav Damu <pranavdamu@gmail.com>
2025-02-21 19:53:34 -0600

    new implementation - not working

--- Q5-new/dissemination.go
+++ Q5-new/dissemination.go
@@ -1,3 +1,220 @@
+// package main
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	"log"
+// 	"net"
+// 	"os"
+// 	"strconv"
+// 	"strings"
+// 	"sync"
+
+// 	pb "github.com/pranavsdamu/damu-swim-disseem/generated/swim"
+
+// 	"google.golang.org/grpc"
+// )
+
+// // DisseminationServer implements the SwimService gRPC interface for dissemination.
+// type DisseminationServer struct {
+// 	pb.UnimplementedSwimServiceServer
+
+// 	nodeID          int32
+// 	membershipMutex sync.RWMutex
+// 	membershipList  []*pb.NodeInfo
+// }
+
+// // NewDisseminationServer constructs a new DisseminationServer.
+// func NewDisseminationServer(nodeID int32, membershipList []*pb.NodeInfo) *DisseminationServer {
+// 	return &DisseminationServer{
+// 		nodeID:         nodeID,
+// 		membershipList: membershipList,
+// 	}
+// }
+
+// // Join is called by a new node that wants to join the cluster.
+// func (s *DisseminationServer) Join(ctx context.Context, req *pb.JoinRequest) (*pb.JoinResponse, error) {
+// 	fmt.Printf(
+// 		"Component Dissemination of Node %d runs RPC Join called by Component Dissemination of Node %d\n",
+// 		s.nodeID, req.SenderId,
+// 	)
+
+// 	// Add the new node if not already present.
+// 	newNode := &pb.NodeInfo{
+// 		NodeId: req.SenderId,
+// 		Host:   req.Host,
+// 		Port:   req.Port,
+// 	}
+
+// 	s.membershipMutex.Lock()
+// 	defer s.membershipMutex.Unlock()
+
+// 	for _, n := range s.membershipList {
+// 		if n.NodeId == req.SenderId {
+// 			return &pb.JoinResponse{
+// 				MembershipList: s.membershipList,
+// 			}, nil
+// 		}
+// 	}
+
+// 	s.membershipList = append(s.membershipList, newNode)
+// 	return &pb.JoinResponse{
+// 		MembershipList: s.membershipList,
+// 	}, nil
+// }
+
+// // BroadcastFailure is invoked when a node detects a failure.
+// // It updates the local membership list and, if this node detected the failure,
+// // multicasts the failure info to all other nodes.
+// func (s *DisseminationServer) BroadcastFailure(ctx context.Context, req *pb.BroadcastFailureRequest) (*pb.BroadcastFailureResponse, error) {
+// 	fmt.Printf(
+// 		"Component Dissemination of Node %d runs RPC BroadcastFailure called by Component Dissemination of Node %d\n",
+// 		s.nodeID, req.SenderId,
+// 	)
+
+// 	// Update local membership: remove the failed node.
+// 	s.membershipMutex.Lock()
+// 	var updated []*pb.NodeInfo
+// 	for _, n := range s.membershipList {
+// 		if n.NodeId != req.FailedNodeId {
+// 			updated = append(updated, n)
+// 		}
+// 	}
+// 	s.membershipList = updated
+// 	s.membershipMutex.Unlock()
+
+// 	// If this node is the one that detected the failure (i.e. sender equals self),
+// 	// then multicast the failure info to all other nodes.
+// 	if req.SenderId == s.nodeID {
+// 		go s.BroadcastFailureToAll(req.FailedNodeId)
+// 	}
+
+// 	return &pb.BroadcastFailureResponse{Success: true}, nil
+// }
+
+// // BroadcastFailureToAll sends the failure information to all other nodes.
+// func (s *DisseminationServer) BroadcastFailureToAll(failedNodeId int32) {
+// 	s.membershipMutex.RLock()
+// 	defer s.membershipMutex.RUnlock()
+
+// 	for _, node := range s.membershipList {
+// 		if node.NodeId == s.nodeID {
+// 			continue
+// 		}
+
+// 		address := fmt.Sprintf("%s:%d", node.Host, node.Port)
+// 		fmt.Printf(
+// 			"Component Dissemination of Node %d sends RPC BroadcastFailure to Component Dissemination of Node %d\n",
+// 			s.nodeID, node.NodeId,
+// 		)
+
+// 		conn, err := grpc.Dial(address, grpc.WithInsecure())
+// 		if err != nil {
+// 			log.Printf("Failed to connect to node %d for BroadcastFailure: %v", node.NodeId, err)
+// 			continue
+// 		}
+
+// 		client := pb.NewSwimServiceClient(conn)
+// 		_, err = client.BroadcastFailure(context.Background(), &pb.BroadcastFailureRequest{
+// 			SenderId:     s.nodeID,
+// 			FailedNodeId: failedNodeId,
+// 		})
+// 		conn.Close()
+
+// 		if err != nil {
+// 			log.Printf("BroadcastFailure RPC to node %d failed: %v", node.NodeId, err)
+// 		}
+// 	}
+// }
+
+// // JoinCluster allows a new node to join the cluster by contacting a bootstrap node.
+// func (s *DisseminationServer) JoinCluster(bootstrapHost string, bootstrapPort int, myHost string, myPort int, myNodeID int32) {
+// 	address := fmt.Sprintf("%s:%d", bootstrapHost, bootstrapPort)
+// 	fmt.Printf(
+// 		"Component Dissemination of Node %d sends RPC Join to Component Dissemination of Node (bootstrap)\n",
+// 		myNodeID,
+// 	)
+
+// 	conn, err := grpc.Dial(address, grpc.WithInsecure())
+// 	if err != nil {
+// 		log.Printf("Unable to dial bootstrap node: %v", err)
+// 		return
+// 	}
+// 	defer conn.Close()
+
+// 	client := pb.NewSwimServiceClient(conn)
+// 	res, err := client.Join(context.Background(), &pb.JoinRequest{
+// 		SenderId: myNodeID,
+// 		Host:     myHost,
+// 		Port:     int32(myPort),
+// 	})
+// 	if err != nil {
+// 		log.Printf("Failed to call Join on bootstrap node: %v", err)
+// 		return
+// 	}
+
+// 	s.membershipMutex.Lock()
+// 	s.membershipList = res.MembershipList
+// 	s.membershipMutex.Unlock()
+// }
+
+// // main starts the gRPC server for the Dissemination component.
+// func main() {
+// 	nodeIDStr := os.Getenv("NODE_ID")
+// 	portStr := os.Getenv("DISSEM_PORT")
+// 	membershipStr := os.Getenv("MEMBERSHIP") // e.g., "1:node1:50051,2:node2:50052,3:node3:50053"
+
+// 	var nodeID int32 = 1
+// 	if nodeIDStr != "" {
+// 		if val, err := strconv.Atoi(nodeIDStr); err == nil {
+// 			nodeID = int32(val)
+// 		}
+// 	}
+
+// 	dissemPort := 6000
+// 	if portStr != "" {
+// 		if val, err := strconv.Atoi(portStr); err == nil {
+// 			dissemPort = val
+// 		}
+// 	}
+
+// 	var membershipList []*pb.NodeInfo
+// 	if membershipStr != "" {
+// 		nodes := strings.Split(membershipStr, ",")
+// 		for _, n := range nodes {
+// 			parts := strings.Split(n, ":")
+// 			if len(parts) == 3 {
+// 				if id, errID := strconv.Atoi(parts[0]); errID == nil {
+// 					if prt, errPort := strconv.Atoi(parts[2]); errPort == nil {
+// 						membershipList = append(membershipList, &pb.NodeInfo{
+// 							NodeId: int32(id),
+// 							Host:   parts[1],
+// 							Port:   int32(prt),
+// 						})
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	srv := NewDisseminationServer(nodeID, membershipList)
+
+// 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", dissemPort))
+// 	if err != nil {
+// 		log.Fatalf("Failed to listen on port %d: %v", dissemPort, err)
+// 	}
+
+// 	grpcServer := grpc.NewServer()
+// 	pb.RegisterSwimServiceServer(grpcServer, srv)
+
+// 	log.Printf("Dissemination component of Node %d listening on port %d", nodeID, dissemPort)
+// 	if err := grpcServer.Serve(lis); err != nil {
+// 		log.Fatalf("Failed to serve gRPC: %v", err)
+// 	}
+// }
+
+// -------------------------
+
 package main
 
 import (
@@ -9,37 +226,44 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	pb "github.com/pranavsdamu/damu-swim-disseem/generated/swim"
-
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/grpclog"
 )
 
-// DisseminationServer implements the SwimService gRPC interface for dissemination.
-type DisseminationServer struct {
-	pb.UnimplementedSwimServiceServer
+// Failure detection tuning parameters
+const (
+	PingTimeout      = 3 * time.Second // Increased timeout to reduce false positives
+	IndirectPingTime = 1 * time.Second
+	FailureThreshold = 3 // Number of missed pings before marking failed
+)
 
-	nodeID          int32
-	membershipMutex sync.RWMutex
-	membershipList  []*pb.NodeInfo
+// DisseminationServer implements the SwimService gRPC interface.
+type DisseminationServer struct {
+	pb.UnimplementedSwimServiceServer // Ensures all RPC methods exist
+	nodeID                            int32
+	membershipMutex                   sync.RWMutex
+	membershipList                    []*pb.NodeInfo
 }
 
 // NewDisseminationServer constructs a new DisseminationServer.
 func NewDisseminationServer(nodeID int32, membershipList []*pb.NodeInfo) *DisseminationServer {
+	if membershipList == nil {
+		membershipList = []*pb.NodeInfo{} // Ensure non-nil slice
+	}
 	return &DisseminationServer{
 		nodeID:         nodeID,
 		membershipList: membershipList,
 	}
 }
 
-// Join is called by a new node that wants to join the cluster.
+// Join is called when a new node wants to join the cluster.
 func (s *DisseminationServer) Join(ctx context.Context, req *pb.JoinRequest) (*pb.JoinResponse, error) {
-	fmt.Printf(
-		"Component Dissemination of Node %d runs RPC Join called by Component Dissemination of Node %d\n",
-		s.nodeID, req.SenderId,
-	)
+	log.Printf("[JOIN RPC] Node %d received Join request from Node %d\n", s.nodeID, req.SenderId)
 
-	// Add the new node if not already present.
 	newNode := &pb.NodeInfo{
 		NodeId: req.SenderId,
 		Host:   req.Host,
@@ -49,42 +273,49 @@ func (s *DisseminationServer) Join(ctx context.Context, req *pb.JoinRequest) (*p
 	s.membershipMutex.Lock()
 	defer s.membershipMutex.Unlock()
 
+	// Check if node is already in the list
 	for _, n := range s.membershipList {
 		if n.NodeId == req.SenderId {
-			return &pb.JoinResponse{
-				MembershipList: s.membershipList,
-			}, nil
+			return &pb.JoinResponse{MembershipList: s.membershipList}, nil
 		}
 	}
 
+	// Add new node to the membership list
 	s.membershipList = append(s.membershipList, newNode)
-	return &pb.JoinResponse{
-		MembershipList: s.membershipList,
-	}, nil
+	log.Printf("[MEMBERSHIP] Node %d added Node %d\n", s.nodeID, req.SenderId)
+
+	return &pb.JoinResponse{MembershipList: s.membershipList}, nil
 }
 
-// BroadcastFailure is invoked when a node detects a failure.
-// It updates the local membership list and, if this node detected the failure,
-// multicasts the failure info to all other nodes.
+// BroadcastFailure handles failure detection and propagates it to all nodes.
 func (s *DisseminationServer) BroadcastFailure(ctx context.Context, req *pb.BroadcastFailureRequest) (*pb.BroadcastFailureResponse, error) {
-	fmt.Printf(
-		"Component Dissemination of Node %d runs RPC BroadcastFailure called by Component Dissemination of Node %d\n",
-		s.nodeID, req.SenderId,
-	)
+	log.Printf("[BROADCAST RPC RECEIVED] Node %d received BroadcastFailure for Node %d from Node %d", s.nodeID, req.FailedNodeId, req.SenderId)
 
-	// Update local membership: remove the failed node.
+	// Lock membership list
 	s.membershipMutex.Lock()
-	var updated []*pb.NodeInfo
-	for _, n := range s.membershipList {
-		if n.NodeId != req.FailedNodeId {
-			updated = append(updated, n)
+	defer s.membershipMutex.Unlock()
+
+	// Remove the failed node from the list
+	var updatedList []*pb.NodeInfo
+	found := false
+
+	for _, node := range s.membershipList {
+		if node.NodeId == req.FailedNodeId {
+			found = true
+		} else {
+			updatedList = append(updatedList, node)
 		}
 	}
-	s.membershipList = updated
-	s.membershipMutex.Unlock()
 
-	// If this node is the one that detected the failure (i.e. sender equals self),
-	// then multicast the failure info to all other nodes.
+	s.membershipList = updatedList
+
+	if found {
+		log.Printf("[MEMBERSHIP] Node %d removed Node %d", s.nodeID, req.FailedNodeId)
+	} else {
+		log.Printf("[INFO] Node %d had already removed Node %d", s.nodeID, req.FailedNodeId)
+	}
+
+	// Propagate failure if it was detected locally
 	if req.SenderId == s.nodeID {
 		go s.BroadcastFailureToAll(req.FailedNodeId)
 	}
@@ -92,77 +323,53 @@ func (s *DisseminationServer) BroadcastFailure(ctx context.Context, req *pb.Broa
 	return &pb.BroadcastFailureResponse{Success: true}, nil
 }
 
-// BroadcastFailureToAll sends the failure information to all other nodes.
+// BroadcastFailureToAll propagates the failure to all known nodes.
 func (s *DisseminationServer) BroadcastFailureToAll(failedNodeId int32) {
 	s.membershipMutex.RLock()
 	defer s.membershipMutex.RUnlock()
 
 	for _, node := range s.membershipList {
-		if node.NodeId == s.nodeID {
+		if node.NodeId == s.nodeID { // Skip self
 			continue
 		}
 
 		address := fmt.Sprintf("%s:%d", node.Host, node.Port)
-		fmt.Printf(
-			"Component Dissemination of Node %d sends RPC BroadcastFailure to Component Dissemination of Node %d\n",
-			s.nodeID, node.NodeId,
-		)
+		log.Printf("[BROADCAST] Node %d sending BroadcastFailure to Node %d", s.nodeID, node.NodeId)
 
-		conn, err := grpc.Dial(address, grpc.WithInsecure())
+		conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 		if err != nil {
-			log.Printf("Failed to connect to node %d for BroadcastFailure: %v", node.NodeId, err)
+			log.Printf("[ERROR] Could not connect to Node %d: %v", node.NodeId, err)
 			continue
 		}
 
 		client := pb.NewSwimServiceClient(conn)
-		_, err = client.BroadcastFailure(context.Background(), &pb.BroadcastFailureRequest{
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		_, err = client.BroadcastFailure(ctx, &pb.BroadcastFailureRequest{
 			SenderId:     s.nodeID,
 			FailedNodeId: failedNodeId,
 		})
-		conn.Close()
+		cancel()
+		conn.Close() // Close connection after RPC call
 
 		if err != nil {
-			log.Printf("BroadcastFailure RPC to node %d failed: %v", node.NodeId, err)
+			log.Printf("[ERROR] BroadcastFailure RPC to Node %d failed: %v", node.NodeId, err)
+		} else {
+			log.Printf("[SUCCESS] BroadcastFailure successfully sent to Node %d", node.NodeId)
 		}
 	}
 }
 
-// JoinCluster allows a new node to join the cluster by contacting a bootstrap node.
-func (s *DisseminationServer) JoinCluster(bootstrapHost string, bootstrapPort int, myHost string, myPort int, myNodeID int32) {
-	address := fmt.Sprintf("%s:%d", bootstrapHost, bootstrapPort)
-	fmt.Printf(
-		"Component Dissemination of Node %d sends RPC Join to Component Dissemination of Node (bootstrap)\n",
-		myNodeID,
-	)
-
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Printf("Unable to dial bootstrap node: %v", err)
-		return
-	}
-	defer conn.Close()
-
-	client := pb.NewSwimServiceClient(conn)
-	res, err := client.Join(context.Background(), &pb.JoinRequest{
-		SenderId: myNodeID,
-		Host:     myHost,
-		Port:     int32(myPort),
-	})
-	if err != nil {
-		log.Printf("Failed to call Join on bootstrap node: %v", err)
-		return
-	}
-
-	s.membershipMutex.Lock()
-	s.membershipList = res.MembershipList
-	s.membershipMutex.Unlock()
+// gRPC Debug Logging
+func init() {
+	grpclog.SetLoggerV2(grpclog.NewLoggerV2(os.Stdout, os.Stdout, os.Stderr))
+	log.Println("[DEBUG] gRPC Debugging Enabled!")
 }
 
-// main starts the gRPC server for the Dissemination component.
+// main starts the gRPC server.
 func main() {
 	nodeIDStr := os.Getenv("NODE_ID")
 	portStr := os.Getenv("DISSEM_PORT")
-	membershipStr := os.Getenv("MEMBERSHIP") // e.g., "1:node1:50051,2:node2:50052,3:node3:50053"
+	membershipStr := os.Getenv("MEMBERSHIP")
 
 	var nodeID int32 = 1
 	if nodeIDStr != "" {
@@ -184,14 +391,14 @@ func main() {
 		for _, n := range nodes {
 			parts := strings.Split(n, ":")
 			if len(parts) == 3 {
-				if id, errID := strconv.Atoi(parts[0]); errID == nil {
-					if prt, errPort := strconv.Atoi(parts[2]); errPort == nil {
-						membershipList = append(membershipList, &pb.NodeInfo{
-							NodeId: int32(id),
-							Host:   parts[1],
-							Port:   int32(prt),
-						})
-					}
+				id, errID := strconv.Atoi(parts[0])
+				prt, errPort := strconv.Atoi(parts[2])
+				if errID == nil && errPort == nil {
+					membershipList = append(membershipList, &pb.NodeInfo{
+						NodeId: int32(id),
+						Host:   parts[1],
+						Port:   int32(prt),
+					})
 				}
 			}
 		}
@@ -201,14 +408,14 @@ func main() {
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", dissemPort))
 	if err != nil {
-		log.Fatalf("Failed to listen on port %d: %v", dissemPort, err)
+		log.Fatalf("[ERROR] Failed to listen on port %d: %v", dissemPort, err)
 	}
 
 	grpcServer := grpc.NewServer()
 	pb.RegisterSwimServiceServer(grpcServer, srv)
 
-	log.Printf("Dissemination component of Node %d listening on port %d", nodeID, dissemPort)
+	log.Printf("[STARTED] Dissemination component of Node %d is listening on port %d", nodeID, dissemPort)
 	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve gRPC: %v", err)
+		log.Fatalf("[ERROR] gRPC server failed: %v", err)
 	}
 }
--- Q5-new/failure_detector.py
+++ Q5-new/failure_detector.py
@@ -1,3 +1,117 @@
+# import random
+# import time
+# import grpc
+# import swim_pb2
+# import swim_pb2_grpc
+
+# class FailureDetector:
+#     def __init__(self, node_id, membership_list, T=5, k=3):
+#         """
+#         :param node_id: ID of this node.
+#         :param membership_list: List of NodeInfo objects describing all known nodes.
+#         :param T: Ping interval (seconds).
+#         :param k: Number of nodes to contact indirectly if direct ping fails.
+#         """
+#         self.node_id = node_id
+#         self.membership_list = membership_list
+#         self.T = T
+#         self.k = k
+
+#     def run(self):
+#         """
+#         Main loop for the Failure Detector. Repeatedly selects a random target node
+#         to Ping, and sleeps T seconds between iterations.
+#         """
+#         while True:
+#             time.sleep(self.T)
+
+#             # If there's only 1 or 0 nodes in membership, no need to ping
+#             if len(self.membership_list) <= 1:
+#                 continue
+
+#             target_node = random.choice(self.membership_list)
+#             # Avoid self-ping
+#             if target_node.node_id != self.node_id:
+#                 self.ping_node(target_node)
+
+#     def ping_node(self, target_node):
+#         """
+#         Attempt a direct Ping to the given target node.
+#         If that fails, try indirect pings through up to k other nodes.
+#         """
+#         print(f"Component FailureDetector of Node {self.node_id} sends RPC Ping to "
+#               f"Component FailureDetector of Node {target_node.node_id}", flush=True)
+
+#         # Attempt direct ping
+#         try:
+#             with grpc.insecure_channel(f"{target_node.host}:{target_node.port}") as channel:
+#                 stub = swim_pb2_grpc.SwimServiceStub(channel)
+#                 stub.Ping(swim_pb2.PingRequest(sender_id=self.node_id))
+
+#             # If successful:
+#             print(f"Node {target_node.node_id} is healthy", flush=True)
+
+#         except grpc.RpcError:
+#             # If direct ping fails, attempt indirect
+#             self.indirect_ping(target_node)
+
+#     def indirect_ping(self, target_node):
+#         """
+#         Attempt an IndirectPing to the target_node via up to k other nodes in membership.
+#         If all indirect pings fail, declare the target node as failed.
+#         """
+#         # Filter out ourselves and the target node from the potential 'helpers'
+#         other_nodes = [node for node in self.membership_list
+#                        if node.node_id not in (self.node_id, target_node.node_id)]
+
+#         # Randomly select up to k of those for indirect ping
+#         ping_nodes = random.sample(other_nodes, min(self.k, len(other_nodes)))
+
+#         for helper_node in ping_nodes:
+#             print(f"Component FailureDetector of Node {self.node_id} sends RPC IndirectPing to "
+#                   f"Component FailureDetector of Node {helper_node.node_id}", flush=True)
+#             try:
+#                 with grpc.insecure_channel(f"{helper_node.host}:{helper_node.port}") as channel:
+#                     stub = swim_pb2_grpc.SwimServiceStub(channel)
+#                     response = stub.IndirectPing(swim_pb2.IndirectPingRequest(
+#                         sender_id=self.node_id,
+#                         target_id=target_node.node_id
+#                     ))
+#                     if response.success:
+#                         print(f"Node {target_node.node_id} is healthy (indirect ping)", flush=True)
+#                         return  # The node is alive, so stop here
+
+#             except grpc.RpcError:
+#                 # If we can’t even contact the helper, continue to the next
+#                 pass
+
+#         # If we exhausted all helpers without success, the target is likely failed
+#         self.mark_node_failed(target_node)
+
+#     def mark_node_failed(self, target_node):
+#         """
+#         Remove the failed node from membership and inform the local Go Dissemination
+#         component that the node has failed.
+#         """
+#         print(f"Node {target_node.node_id} has failed", flush=True)
+#         if target_node in self.membership_list:
+#             self.membership_list.remove(target_node)
+
+#         # Notify local Dissemination service so it can broadcast the failure
+#         try:
+#             # Assuming your Go Dissemination is listening on localhost:6000 in the same container
+#             with grpc.insecure_channel("localhost:6000") as channel:
+#                 stub = swim_pb2_grpc.SwimServiceStub(channel)
+#                 print(f"Component FailureDetector of Node {self.node_id} sends RPC BroadcastFailure "
+#                       f"to Component Dissemination of Node {self.node_id}", flush=True)
+#                 stub.BroadcastFailure(
+#                     swim_pb2.BroadcastFailureRequest(
+#                         sender_id=self.node_id,
+#                         failed_node_id=target_node.node_id
+#                     )
+#                 )
+#         except grpc.RpcError as e:
+#             print(f"Error calling BroadcastFailure on local Go server: {e}", flush=True)
 import random
 import time
 import grpc
@@ -39,8 +153,7 @@ class FailureDetector:
         Attempt a direct Ping to the given target node.
         If that fails, try indirect pings through up to k other nodes.
         """
-        print(f"Component FailureDetector of Node {self.node_id} sends RPC Ping to "
-              f"Component FailureDetector of Node {target_node.node_id}", flush=True)
+        print(f"[PING] Node {self.node_id} → Node {target_node.node_id}", flush=True)
 
         # Attempt direct ping
         try:
@@ -49,9 +162,10 @@ class FailureDetector:
                 stub.Ping(swim_pb2.PingRequest(sender_id=self.node_id))
 
             # If successful:
-            print(f"Node {target_node.node_id} is healthy", flush=True)
+            print(f"[HEALTHY] Node {target_node.node_id} responded", flush=True)
 
-        except grpc.RpcError:
+        except grpc.RpcError as e:
+            print(f"[WARNING] Node {target_node.node_id} did not respond! Attempting indirect ping...", flush=True)
             # If direct ping fails, attempt indirect
             self.indirect_ping(target_node)
 
@@ -61,15 +175,13 @@ class FailureDetector:
         If all indirect pings fail, declare the target node as failed.
         """
         # Filter out ourselves and the target node from the potential 'helpers'
-        other_nodes = [node for node in self.membership_list
-                       if node.node_id not in (self.node_id, target_node.node_id)]
+        other_nodes = [node for node in self.membership_list if node.node_id not in (self.node_id, target_node.node_id)]
 
         # Randomly select up to k of those for indirect ping
         ping_nodes = random.sample(other_nodes, min(self.k, len(other_nodes)))
 
         for helper_node in ping_nodes:
-            print(f"Component FailureDetector of Node {self.node_id} sends RPC IndirectPing to "
-                  f"Component FailureDetector of Node {helper_node.node_id}", flush=True)
+            print(f"[INDIRECT PING] Node {self.node_id} → Node {helper_node.node_id} → Node {target_node.node_id}", flush=True)
             try:
                 with grpc.insecure_channel(f"{helper_node.host}:{helper_node.port}") as channel:
                     stub = swim_pb2_grpc.SwimServiceStub(channel)
@@ -78,12 +190,11 @@ class FailureDetector:
                         target_id=target_node.node_id
                     ))
                     if response.success:
-                        print(f"Node {target_node.node_id} is healthy (indirect ping)", flush=True)
+                        print(f"[INDIRECT SUCCESS] Node {target_node.node_id} is healthy", flush=True)
                         return  # The node is alive, so stop here
 
-            except grpc.RpcError:
-                # If we can’t even contact the helper, continue to the next
-                pass
+            except grpc.RpcError as e:
+                print(f"[ERROR] Indirect ping via Node {helper_node.node_id} failed: {e}", flush=True)
 
         # If we exhausted all helpers without success, the target is likely failed
         self.mark_node_failed(target_node)
@@ -93,22 +204,27 @@ class FailureDetector:
         Remove the failed node from membership and inform the local Go Dissemination
         component that the node has failed.
         """
-        print(f"Node {target_node.node_id} has failed", flush=True)
+        print(f"[FAILURE DETECTED] Node {target_node.node_id} has failed", flush=True)
+
+        # Remove from local membership
         if target_node in self.membership_list:
             self.membership_list.remove(target_node)
 
-        # Notify local Dissemination service so it can broadcast the failure
+        # Notify local Dissemination component to broadcast the failure
         try:
-            # Assuming your Go Dissemination is listening on localhost:6000 in the same container
             with grpc.insecure_channel("localhost:6000") as channel:
                 stub = swim_pb2_grpc.SwimServiceStub(channel)
-                print(f"Component FailureDetector of Node {self.node_id} sends RPC BroadcastFailure "
-                      f"to Component Dissemination of Node {self.node_id}", flush=True)
-                stub.BroadcastFailure(
+                print(f"[BROADCAST] Node {self.node_id} → Dissemination: BroadcastFailure for Node {target_node.node_id}", flush=True)
+                response = stub.BroadcastFailure(
                     swim_pb2.BroadcastFailureRequest(
                         sender_id=self.node_id,
                         failed_node_id=target_node.node_id
                     )
                 )
+                if response.success:
+                    print(f"[BROADCAST SUCCESS] Node {target_node.node_id} failure broadcasted", flush=True)
+                else:
+                    print(f"[BROADCAST FAILED] Dissemination did not acknowledge failure", flush=True)
+
         except grpc.RpcError as e:
-            print(f"Error calling BroadcastFailure on local Go server: {e}", flush=True)
+            print(f"[ERROR] BroadcastFailure RPC failed: {e}", flush=True)
